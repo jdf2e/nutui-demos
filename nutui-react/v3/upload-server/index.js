382a95d2ff188bf6c3d4a894209a5a40766048fd
Alex.hxy <1872591453@qq.com>
2024-12-11 15:06:50 +0800

    feat: uploader server demo

--- nutui-react/v3/upload-server/index.js
+++ nutui-react/v3/upload-server/index.js
@@ -3,13 +3,12 @@ const multer = require('multer')
 const cors = require('cors')
 const path = require('path')
 
-// 初始化 Express 应用
 const app = express()
 const port = 3000
 
 app.use(
   cors({
-    origin: '*', // 或者 '*'
+    origin: '*', 
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
@@ -28,16 +27,13 @@ const storage = multer.diskStorage({
   },
 })
 
-// 创建`multer`实例
 const upload = multer({ storage: storage })
 
 // 设置路由来处理文件上传
 app.post('/upload', upload.single('file'), (req, res) => {
   try {
-    // 文件信息可以通过`req.file`访问
     console.log(req.file)
     const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`
-    // 返回成功响应
     res.status(200).send({ msg: 'File uploaded successfully', url: fileUrl })
   } catch (error) {
     res.status(400).send('Error uploading file')
